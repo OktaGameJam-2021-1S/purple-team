10f45e38c39f63c1bc5eadb7bdb0efed8f1da79d
Lucas Tomé Avelino Câmara <lucas.tacamara@gmail.com>
2021-06-05 14:35:31 -0300

    [Added] Light loss over time and light refill spot

---
+++ Unity/Assets/Scripts/GamePlay/InputController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GamePlay
+{
+    public class PlayerInput
+    {
+        public Vector2 axis;
+        public bool interactButton;
+
+        public void ClearInput()
+        {
+            axis = Vector2.zero;
+            interactButton = false;
+        }
+    }
+
+    public class InputController : MonoBehaviour
+    {
+        [SerializeField] private float _falloffAxis = 4f;
+        private PlayerInput _playerInput;
+        private Vector2 _targetAxis;
+
+        public PlayerInput PlayerInput
+        {
+            get
+            {
+                return _playerInput;
+            }
+        }
+
+        public void Initialize()
+        {
+            _playerInput = new PlayerInput();
+        }
+
+        public void UpdatePlayerInput()
+        {
+            _targetAxis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (_targetAxis.magnitude > 1)
+            {
+                _targetAxis.Normalize();
+            }
+
+            if (_targetAxis.x == 0 && _targetAxis.y == 0)
+            {
+                _playerInput.axis = Vector2.MoveTowards(_playerInput.axis, _targetAxis, _falloffAxis * Time.deltaTime);
+            }
+            else
+            {
+                _playerInput.axis = _targetAxis;
+            }
+
+            _playerInput.interactButton = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0);
+        }        
+    }
+}
--- Unity/Assets/Scripts/GamePlay/PlayerController.cs
+++ Unity/Assets/Scripts/GamePlay/PlayerController.cs
@@ -7,7 +7,7 @@ using Utils;
 
 namespace GamePlay
 {
-    public class PlayerController : MonoBehaviourPun
+    public class PlayerController : MonoBehaviourPun, IPunObservable
     {
         [SerializeField] private MovementController _movementController;
         [SerializeField] private PlayerLightController _playerLightController;
@@ -26,14 +26,40 @@ namespace GamePlay
         /// Process player input
         /// </summary>
         /// <param name="axis">Input axis</param>
-        public void ProcessInput(Vector2 axis)
+        public void ProcessInput(Vector2 axis, bool interactButton)
         {
             _movementController.UpdateAxis(axis);
+            _playerLightController.ProcessInput(interactButton);
         }
 
         public void UpdatePlayer(float deltaTime)
         {
+            _playerLightController.UpdateLight(deltaTime);
+        }
+
+        public void UpdatePlayerPosition(float deltaTime)
+        {
             _movementController.UpdatePosition(deltaTime);
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(_playerLightController.IsLightOn);
+                stream.SendNext(_playerLightController.CurrentLightPower);
+                stream.SendNext(PhotonNetwork.ServerTimestamp);
+            }
+            else
+            {
+                bool isLightOn = (bool) stream.ReceiveNext();
+                float lightPower = (float) stream.ReceiveNext();
+                int sendTimeStamp = (int) stream.ReceiveNext();
+
+                float deltaTime = Utilities.DeltaTime(sendTimeStamp, PhotonNetwork.ServerTimestamp);
+
+                _playerLightController.SyncLight(isLightOn, lightPower, deltaTime);
+            }
         }
     }
 }
